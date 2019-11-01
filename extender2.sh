603a488232efe0d6d8ae3513fd3d81aa5537fe0f
Anton Teplov <a.teplov2012@gmail.com>
2019-11-01 15:21:07 +0300

    add extender2

---
+++ extender2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat  > /dev/shm/tmp1
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>1) print $1," ",$2 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>2) print $1," ",$3 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>3) print $1," ",$4 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>4) print $1," ",$5 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>5) print $1," ",$6 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>6) print $1," ",$7 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>7) print $1," ",$8 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>8) print $1," ",$9 }'
+cat /dev/shm/tmp1 | awk -F'/' '{ if(NF>9) print $1," ",$10 }'
